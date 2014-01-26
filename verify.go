b26ee26ba094ec5c92305681d682428671d31356
datastream <guxianjie@gmail.com>
2014-01-26 20:48:38 +0800

    add verify func

---
+++ verify.go
@@ -0,0 +1,75 @@
+package sign4
+
+// Verify AWS Canonical Request For Signature Version 4
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// Authorization: AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=content-type;date;host, Signature=5a15b22cf462f047318703b92e6f4f38884e4a7ab7b1d6426ca46a8bd1c26cbc
+func GetSignature(r *http.Request) (*Signature, string, error) {
+	authHeader := r.Header.Get("Authorization")
+	if len(authHeader) < 16 {
+		return nil, "", errors.New("wrong authorization header")
+	}
+	if authHeader[:16] != "AWS4-HMAC-SHA256" {
+		return nil, "", errors.New("wrong authorization header")
+	}
+	pattens := strings.Split(authHeader, " ")
+	if len(pattens) != 4 {
+		return nil, "", errors.New("wrong authorization header")
+	}
+	signature, err := getCredential(pattens[1][:len(pattens[1])-1])
+	if err != nil {
+		return nil, "", errors.New("wrong authorization header")
+	}
+	signedHeaders, err := getSignedHeaders(pattens[2][:len(pattens[2])-1])
+	if err != nil {
+		return nil, "", errors.New("wrong authorization header")
+	}
+	for k := range r.Header {
+		if signedHeaders[k] != true {
+			r.Header.Del(k)
+		}
+	}
+	if pattens[3][:9] != "Signature" {
+		return nil, "", errors.New("wrong authorization header")
+	}
+	return signature, authHeader, nil
+}
+
+func getCredential(s string) (*Signature, error) {
+	if s[:11] != "Credential=" {
+		return nil, errors.New("wrong credential part")
+	}
+	parts := strings.Split(s[11:], "/")
+	if len(parts) != 5 {
+		return nil, errors.New("wrong credential part")
+	}
+	if parts[4] != "aws4_request" {
+		return nil, errors.New("wrong credential part")
+	}
+	ss := &Signature{
+		AccessKey: parts[0],
+		Region:    parts[2],
+		Service:   parts[3],
+	}
+	return ss, nil
+}
+
+func getSignedHeaders(s string) (map[string]bool, error) {
+	headers := strings.Split(s[14:], ";")
+	h := make(map[string]bool)
+	if len(s) < 14 {
+		return h, errors.New("wrong signedheaders part")
+	}
+	if s[:14] != "SignedHeaders=" {
+		return h, errors.New("wrong signedheaders part")
+	}
+	for _, v := range headers {
+		h[v] = true
+	}
+	return h, nil
+}
---
+++ verify_test.go
@@ -0,0 +1,31 @@
+package sign4_test
+
+import (
+	"../aws"
+	"net/http"
+	"testing"
+)
+
+func TestVerify(t *testing.T) {
+	s := sign4.Signature{
+		AccessKey: "AKIDEXAMPLE",
+		SecretKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY",
+		Region:    "us-east-1",
+		Service:   "host",
+	}
+	r, _ := http.NewRequest("GET", "http://host.foo.com/%20/foo", nil)
+	r.Header.Add("date", "Mon, 09 Sep 2011 23:36:00 GMT")
+	s.SignRequest(r)
+	r.Header.Add("x-data", "testmemmmm")
+	authheader := r.Header.Get("authorization")
+	ss, aa, err := sign4.GetSignature(r)
+	if err != nil {
+		t.Fatal("failed to get signature")
+	}
+	if ss.AccessKey != s.AccessKey || ss.Region != s.Region || ss.Service != s.Service {
+		t.Fatal("failed to get signature", ss.AccessKey, ss.Region, ss.Service)
+	}
+	if aa != authheader {
+		t.Fatal("wrong authorization header", aa)
+	}
+}
