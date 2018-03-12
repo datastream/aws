23856c1fc8f0208d907a41b2474c911b8b89a1fe
datastream <guxianjie@gmail.com>
2018-03-12 16:59:02 +0800

    fix header split

--- verify.go
+++ verify.go
@@ -9,6 +9,7 @@ import (
 )
 
 // Authorization: AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=content-type;date;host, Signature=5a15b22cf462f047318703b92e6f4f38884e4a7ab7b1d6426ca46a8bd1c26cbc
+//Authorization: AWS4-HMAC-SHA256 Credential=devops/20180312/hz/dnsapi/aws4_request,SignedHeaders=Content-Length;Content-type;host;x-amz-date,Signature=8a31f6aaa5026579bb2cf20962768190fdd0b4846ed5c48842fa61936245e9c5
 func GetSignature(r *http.Request) (*Signature, string, map[string]bool, error) {
 	authHeader := r.Header.Get("Authorization")
 	return GetSignatureFromString(authHeader)
@@ -22,15 +23,25 @@ func GetSignatureFromString(authHeader string) (*Signature, string, map[string]b
 	if authHeader[:16] != "AWS4-HMAC-SHA256" {
 		return nil, "", signedHeaders, errors.New("get aws4-hmac-sha256 failed")
 	}
-	pattens := strings.Split(authHeader, " ")
+	items := strings.Split(authHeader, " ")
+	var pattens []string
+	for _, v := range items {
+		v1 := strings.Split(v, ",")
+		for _, value := range v1 {
+			if len(strings.Trim(value, " ")) == 0 {
+				continue
+			}
+			pattens = append(pattens, strings.Trim(value, " "))
+		}
+	}
 	if len(pattens) != 4 {
 		return nil, "", signedHeaders, errors.New("wrong authorization header size")
 	}
-	signature, err := getCredential(pattens[1][:len(pattens[1])-1])
+	signature, err := getCredential(pattens[1])
 	if err != nil {
 		return nil, "", signedHeaders, errors.New("get authorization header signature failed")
 	}
-	signedHeaders, err = getSignedHeaders(pattens[2][:len(pattens[2])-1])
+	signedHeaders, err = getSignedHeaders(pattens[2])
 	if err != nil {
 		return nil, "", signedHeaders, errors.New("get authorization header signedHeaders failed")
 	}
